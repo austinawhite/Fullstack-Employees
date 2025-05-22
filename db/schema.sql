defdf6529c7843714994116213f88da469503895
Austina White <austinawhite@gmail.com>
2025-05-22 18:45:40 -0400

    schema updates

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,10 @@
 DROP TABLE IF EXISTS employees;
 
+CREATE Table employees(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    birthday DATE DEFAULT NOW() NOT NULL,
+    salary INTEGER NOT NULL
+);
+
 
